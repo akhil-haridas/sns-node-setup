28faf9242b6c635ab0eeff040c18687edfd068c3
akhil-haridas <4khilharidas@gmail.com>
2024-11-07 18:40:01 +0530

    updated with static key

--- config/webpush.js
+++ config/webpush.js
@@ -1,7 +1,7 @@
 const webPush = require('web-push');
 
-const publicVapidKey = process.env.PUBLIC_VAPID_KEY;
-const privateVapidKey = process.env.PRIVATE_VAPID_KEY;
+const publicVapidKey = process.env.PUBLIC_VAPID_KEY || "BJ4EwHWUsuUNRgHRn_bgNtjXAhIlGZeXtmQB9JdjTHinygmzIGBl9GYIih09IotP3v7k3qESo9RVh825jXUHSZg";
+const privateVapidKey = process.env.PRIVATE_VAPID_KEY || "E7602w8Nll6W3BHVIrnDJKYwC4jiLOh2A44_b8maScE";
 
 webPush.setVapidDetails(
     "mailto:your-email@example.com",
